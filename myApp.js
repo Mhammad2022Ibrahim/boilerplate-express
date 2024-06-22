6c7cd621f7c8aff0df7aed944672506274002d88
Mhammad Ibrahim <103114465+Mhammad2022Ibrahim@users.noreply.github.com>
2024-06-22 19:37:19 +0300

    update boilerplate-express

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
@@ -5,11 +7,25 @@ let app = express();
 console.log("Hello World");
 
 
+// app.get('/',(req,res)=>{
+//     res.send("Hello World");
+// });
+
+absolutePath = __dirname + "/views/index.html";
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+    res.sendFile(absolutePath);
 });
 
+app.use('/public',express.static(__dirname + '/public'));
 
+app.get('/json',(req,res)=>{
+    let message = "Hello Mhammad";
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        message = message.toUpperCase();
+    }
+
+    res.json({"message": message});
+});
 
 
 
