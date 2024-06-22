7a8bcf2bf5c747c31984bbafea2b9359ec1bae99
Mhammad Ibrahim <103114465+Mhammad2022Ibrahim@users.noreply.github.com>
2024-06-22 21:31:48 +0300

    update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
 console.log("Hello World");
 
@@ -12,29 +16,48 @@ console.log("Hello World");
 // });
 
 absolutePath = __dirname + "/views/index.html";
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(absolutePath);
 });
 
-app.use('/public',express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/json',(req,res)=>{
+app.get('/json', (req, res) => {
     let message = "Hello Mhammad";
-    if(process.env.MESSAGE_STYLE === 'uppercase'){
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
         message = message.toUpperCase();
     }
 
-    res.json({"message": message});
+    res.json({ "message": message });
 });
 
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+},
+    (req, res) => {
+        res.json({ "time": req.time })
+    });
 
 
+app.get('/:word/echo', (req, res) => {
+    const word = req.params.word;
+    res.json({ "echo": word });
+});
 
 
-
-
-
+app.route('/name')
+    .get((req, res) => {
+        const firstName = req.query.first;
+        const lastName = req.query.last;
+        res.json({ "name": `${firstName} ${lastName}` });
+    })
+    .post((req, res) => {
+        const firstName = req.body.first;
+        const lastName = req.body.last;
+        res.json({ "name": `${firstName} ${lastName}` });
+    })
 
 
 
